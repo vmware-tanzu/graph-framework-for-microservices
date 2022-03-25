d918f6fb32b70985710c4f4df79ce4988d28a65c
rjanakiraman <rjanakiraman@vmware.com>
2022-03-25 17:07:18 +0530

    fix controllers test

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -29,8 +29,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	confighelloworldcomv1 "kubebuild/api/v1"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -61,9 +59,6 @@ var _ = BeforeSuite(func() {
 	cfg, err := testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
-
-	err = confighelloworldcomv1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
 
 	//+kubebuilder:scaffold:scheme
 
