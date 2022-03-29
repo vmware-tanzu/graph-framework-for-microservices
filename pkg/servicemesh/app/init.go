ee281544668ef0f6d26551fb2f9e9cd4f11ffa5c
rjanakiraman <rjanakiraman@vmware.com>
2022-03-29 20:47:11 +0530

    ignore artifacts and add utils with func

--- pkg/servicemesh/app/init.go
+++ pkg/servicemesh/app/init.go
@@ -2,45 +2,80 @@ package app
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/spf13/cobra"
 
 	"gitlab.eng.vmware.com/nexus/cli/pkg/utils"
 )
 
+type TemplateValues struct {
+	AppName       string
+	ImageRegistry string
+}
+
 var (
-	DMDir          string
-	AppDir         string
-	DatatmodelName string
-	AppName        string
-	RegistryURL    string
+	DMDir            string
+	AppDir           string
+	DatatmodelName   string
+	AppName          string
+	RegistryURL      string
+	DatatmodelImport string
 )
+
+var TEMPLATE_URL = "https://storage.googleapis.com/nexus-template-downloads/app-template.tar"
+var filename = "app-template.tar"
 
 func Init(cmd *cobra.Command, args []string) error {
 	envList := []string{}
-
 	fmt.Println("XXXX:", args)
-
-	if AppDir != "" {
-		envList = append(envList, fmt.Sprintf("APP_DIR=%s", AppDir))
-	}
-	if DMDir != "" {
-		envList = append(envList, fmt.Sprintf("DATAMODEL_DIR=%s", DMDir))
-	}
 	if DatatmodelName != "" {
 		envList = append(envList, fmt.Sprintf("DATAMODEL=%s", DatatmodelName))
 	}
-	if AppName != "" {
-		envList = append(envList, fmt.Sprintf("APPNAME=%s", AppName))
+	var DOWNLOAD_APP string = "true"
+	files, _ := ioutil.ReadDir(".")
+	for _, file := range files {
+		if file.Name() == "PROJECT" {
+			fmt.Printf("Skipping template download and rendering the app directory..")
+			DOWNLOAD_APP = "false"
+		}
 	}
-	if RegistryURL != "" {
-		envList = append(envList, fmt.Sprintf("REGISTRY=%s", RegistryURL))
-	}
+	if DOWNLOAD_APP != "false" {
+		err := utils.DownloadPackage(TEMPLATE_URL, filename)
+		if err != nil {
+			return fmt.Errorf("could not download template files due to %s", err)
+		}
 
-	// cd nexus/
-	err := utils.SystemCommand(envList, "make", "app_init")
-	if err != nil {
-		return err
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		defer file.Close()
+		err = utils.Untar(".", file)
+		if err != nil {
+			return fmt.Errorf("could not unarchive template files due to %s", err)
+		}
+		data := TemplateValues{
+			AppName:       AppName,
+			ImageRegistry: RegistryURL,
+		}
+		err = utils.RenderTemplateFiles(data, ".")
+		if err != nil {
+			return fmt.Errorf("error in rendering template files due to %s", err)
+		}
+	}
+	if DatatmodelName != "" {
+		err := utils.SystemCommand(envList, "make", "init")
+		if err != nil {
+			return fmt.Errorf("error in patching go.mod with datamodel")
+		}
+		err = utils.SystemCommand(envList, "make", "datamodel_init")
+		if err != nil {
+			return fmt.Errorf("error in creating new datamodel due to %s", err)
+		}
 	}
 	return nil
 }
@@ -55,14 +90,16 @@ var InitCmd = &cobra.Command{
 }
 
 func init() {
-	InitCmd.Flags().StringVarP(&DMDir, "datamodel-dir",
-		"m", "", "datamodel directory location.")
-	InitCmd.Flags().StringVarP(&AppDir, "app-dir",
-		"p", "", "app directory location.")
-	InitCmd.Flags().StringVarP(&DatatmodelName, "datamodel",
+	InitCmd.Flags().StringVarP(&DatatmodelName, "datamodel-init",
 		"d", "", "name of the datamodel")
+	InitCmd.Flags().StringVarP(&DatatmodelImport, "datamodel-import",
+		"i", "", "name of the datamodel")
 	InitCmd.Flags().StringVarP(&AppName, "app",
 		"a", "", "name of the application")
 	InitCmd.Flags().StringVarP(&RegistryURL, "registry",
-		"r", "", "registry url to publish docker image")
+		"r", "284299419820.dkr.ecr.us-west-2.amazonaws.com/nexus/playground", "registry url to publish docker image")
+	err := cobra.MarkFlagRequired(InitCmd.Flags(), "app")
+	if err != nil {
+		fmt.Printf("init error: %v\n", err)
+	}
 }
