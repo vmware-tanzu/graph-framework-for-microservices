8a25639bc3e6b789e9f6b39fc866b30e149716b6
Kacper Sawicki <sawickik@vmware.com>
2022-04-13 14:26:07 +0200

    Add GetHttpCodesResponses and new http response example

--- pkg/parser/util.go
+++ pkg/parser/util.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"io/ioutil"
+	"net/http"
 	"path"
 	"regexp"
 
@@ -24,3 +25,36 @@ func SpecialCharsPresent(name string) bool {
 	}
 	return re.MatchString(name)
 }
+
+//TODO: Move this to COMMON nexus repo
+
+// HTTPMethod type.
+type HTTPMethod string
+
+// ResponseCode type.
+type ResponseCode int
+
+// HTTPResponse type.
+type HTTPResponse struct {
+	Description string `json:"description"`
+}
+
+// HTTPCodesResponse code to response type.
+type HTTPCodesResponse map[ResponseCode]HTTPResponse
+
+// HTTPMethodsResponses to response mapping.
+type HTTPMethodsResponses map[HTTPMethod]HTTPCodesResponse
+
+// RestURIs and associated data.
+type RestURIs struct {
+	Uri     string               `json:"uri"`
+	Methods HTTPMethodsResponses `json:"methods"`
+}
+
+type RestAPISpec struct {
+	Uris []RestURIs `json:"uris"`
+}
+
+const DefaultHTTPErrorCode ResponseCode = http.StatusNotImplemented
+
+var DefaultHTTPError = HTTPResponse{Description: http.StatusText(http.StatusNotImplemented)}
