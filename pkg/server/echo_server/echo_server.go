539dee1a57d2d6c6cece2f4d8180115b9a94e94a
Kacper Sawicki <sawickik@vmware.com>
2022-05-11 17:04:18 +0200

    Change swagger url

--- pkg/server/echo_server/echo_server.go
+++ pkg/server/echo_server/echo_server.go
@@ -84,7 +84,7 @@ func (s *EchoServer) RegisterRoutes() {
 		log.Warnf("Could not init swagger-ui fs: %v", err)
 	}
 	swaggerHandler := http.FileServer(fs)
-	s.Echo.GET("/swagger/*", echo.WrapHandler(http.StripPrefix("/swagger/", swaggerHandler)))
+	s.Echo.GET("/docs*", echo.WrapHandler(http.StripPrefix("/swagger/", swaggerHandler)))
 }
 
 func (s *EchoServer) RegisterRouter(restURI nexus.RestURIs) {
@@ -154,6 +154,7 @@ func (s *EchoServer) StopServer() {
 
 func NewEchoServer(conf *config.Config) *EchoServer {
 	e := echo.New()
+	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.CORS())
 
 	return &EchoServer{
