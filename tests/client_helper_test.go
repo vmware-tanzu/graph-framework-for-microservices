37a654928af9bcda6aa5a0ff8ac77b5b25dcb66c
Dilip Tadepalli <tadepallid@vmware.com>
2022-07-28 15:13:43 +0000

    NPT-389 : Add Unit Tests
    Signed-off-by: Dilip Tadepalli <tadepallid@vmware.com>

---
+++ tests/client_helper_test.go
@@ -0,0 +1,81 @@
+//go:build unit
+// +build unit
+
+package tests
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Client is a mock for the controller-runtime dynamic client interface.
+type Client struct {
+	mock.Mock
+}
+
+// ////go:build unit
+// //// +build unit
+
+var _ client.Client = &Client{}
+
+func NewClient() *Client {
+	return &Client{}
+}
+
+func (c *Client) Status() client.StatusWriter {
+	return nil
+}
+
+func (c *Client) Get(ctx context.Context, key types.NamespacedName, obj client.Object) error {
+	args := c.Called(ctx, key, obj)
+	var r0 error
+	if rf, ok := args.Get(0).(func(ctx context.Context, key types.NamespacedName, obj client.Object) error); ok {
+		r0 = rf(ctx, key, obj)
+	} else {
+		r0 = args.Error(0)
+	}
+	return r0
+}
+
+func (c *Client) List(ctx context.Context, list client.ObjectList, opts ...client.ListOption) error {
+	args := c.Called(ctx, list, opts)
+	return args.Error(0)
+}
+
+func (c *Client) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+	args := c.Called(ctx, obj, opts)
+	return args.Error(0)
+}
+func (c *Client) Delete(ctx context.Context, obj client.Object, opts ...client.DeleteOption) error {
+	args := c.Called(ctx, obj, opts)
+	return args.Error(0)
+}
+
+func (c *Client) Update(ctx context.Context, obj client.Object, opts ...client.UpdateOption) error {
+	args := c.Called(ctx, obj, opts)
+	return args.Error(0)
+}
+
+func (c *Client) Patch(ctx context.Context, obj client.Object, patch client.Patch, opts ...client.PatchOption) error {
+	args := c.Called(ctx, obj, patch, opts)
+	return args.Error(0)
+}
+func (c *Client) DeleteAllOf(ctx context.Context, obj client.Object, opts ...client.DeleteAllOfOption) error {
+	args := c.Called(ctx, obj, opts)
+	return args.Error(0)
+}
+
+func (c *Client) Scheme() *runtime.Scheme {
+	args := c.Called()
+	return args.Get(0).(*runtime.Scheme)
+}
+
+func (c *Client) RESTMapper() meta.RESTMapper {
+	args := c.Called()
+	return args.Get(0).(meta.RESTMapper)
+}
