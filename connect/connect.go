8c8eb45aa664eb6c615f857df7b741a383956652
Dinesh Bakiaraj <dbakiaraj@vmware.com>
2022-08-02 15:08:48 +0000

    NPT-279 Api changes to support object and type based replication 
    Nexus connector supports replication of objects from one endpoint to another.
    The ask is to extend this functionality to be able to support:
    1. Type based replication: replication of all objects of a specific type
    2. Replication of object to a specific namespace and/or hierarchy in the destination
       endpoint.
    
    This commit extends the connect api with following attributes:
    1. Ability to specify the source either as a type of a specific object.
    2. Ability to replicate into a hierarchy in the destination datamodel
    3. Ability to repolicate into a specific namespace in the destination datamodel.

--- connect/connect.go
+++ connect/connect.go
@@ -22,6 +22,79 @@ type NexusEndpoint struct {
 	Cert string
 }
 
+type ReplicationStatusEndpoint string
+
+const (
+	Source      ReplicationStatusEndpoint = "Source"
+	Destination ReplicationStatusEndpoint = "Destination"
+)
+
+type SourceKind string
+
+const (
+	Object SourceKind = "Object"
+	Type   SourceKind = "Type"
+)
+
+type KVP struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+// Hierarchy identifies a hierarchy of an object in Nexus Runtime.
+type Hierarchy struct {
+	Labels []KVP `json:"labels"`
+}
+
+// ObjectType identifies a type of objects to be replicated
+type ObjectType struct {
+	Group   string `json:"group"`
+	Kind    string `json:"kind"`
+	Version string `json:"version"`
+}
+
+// ReplicateObject identifies an object in datamodel to be replicated
+type SourceObject struct {
+	// Object type.
+	ObjectType `json:"objectType"`
+
+	// Object name.
+	Name string `json:"name"`
+
+	// If true, the hierarchy of the object is relevant for replication.
+	Hierarchical bool `json:"hierarchical"`
+
+	// Hierarchical path prefix of the object.
+	// This is relevant if the object has to be considered in the context of its hierarchy.
+	// Ignored if value of field Hierarchical is false.
+	Hierarchy Hierarchy `json:"hierarchy,omitempty"`
+}
+
+// ReplicationSource identifies either a single object or all objects of a type
+// that are to be replicated to a destination endpoint.
+type ReplicationSource struct {
+	Kind SourceKind `json:"kind"`
+
+	// Relevant if all objects of a Type are to be replicated.
+	Type ObjectType `json:"type,omitempty"`
+
+	// Relevant if a specific object (and optionally its children) are to be replicated.
+	Object SourceObject `json:"object,omitempty"`
+}
+
+// ReplicationDestination specifies the attributes with which objects are to be created
+// in the destination endpoint.
+type ReplicationDestination struct {
+	// If true, the object will be replicated into the specified hierarchy.
+	Hierarchical bool `json:"hierarchical"`
+
+	// Hierarchy into which the object has to be replicated.
+	Hierarchy Hierarchy `json:"hierarchy,omitempty"`
+
+	// If specified, the replicated object will be scoped to this namespace.
+	Namespace string `json:"namespace,omitempty"`
+}
+
 // ReplicationConfig defines a replication request/instance.
 type ReplicationConfig struct {
 	nexus.Node
@@ -33,32 +106,13 @@ type ReplicationConfig struct {
 	AccessToken string
 
 	// Source of the replication.
-	Source ReplicationObject
+	Source ReplicationSource
 
 	// Destination of the replication.
-	Destination ReplicationObject `json:"destination,omitempty" yaml:"destination,omitempty"`
-}
+	Destination ReplicationDestination
 
-// ReplicationObject identifies a resource in the Nexus Runtime.
-type ReplicationObject struct {
-	// Object identity.
-	Group string `json:"group" yaml:"group"`
-	Kind  string `json:"kind" yaml:"kind"`
-	Name  string `json:"name" yaml:"name"`
-
-	// Identifies if the object is available in local runtime.
-	// If false, the object is available in the remote runtime.
-	LocalRuntime bool `json:"localRuntime,omitempty" yaml:"localRuntime,omitempty"`
-
-	// If true, the hierarchy of the object is relevant for replication.
-	Hierarchical bool `json:"hierarchical,omitempty" yaml:"hierarchical,omitempty"`
-
-	// Hierarchical path prefix of the object.
-	// This is relevant is the object has to be considered in the context of its hierarchy.
-	Hierarchy Hierarchy `json:"hierarchy,omitempty" yaml:"hierarchy,omitempty"`
-}
-
-// Hierarchy identifies a hierarchy of an object in Nexus Runtime.
-type Hierarchy struct {
-	Path string
+	// Endpoint in which status of replication should be captured.
+	// Status can be captured on the corresponding object in source or destination endpoint.
+	// This allows for capturing of status at the endpoint where status is being watched for.
+	StatusEndpoint ReplicationStatusEndpoint
 }
