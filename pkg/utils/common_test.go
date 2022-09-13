a8f325d67bc744dd04bbd03be30721eb0a1d9a30
Kacper Sawicki (c) <sawickik@vmware.com>
2022-09-13 08:55:16 +0000

    [NPT-461] Add unit tests to utils

--- pkg/utils/common_test.go
+++ pkg/utils/common_test.go
@@ -1,7 +1,9 @@
 package utils_test
 
 import (
+	"api-gw/pkg/config"
 	"api-gw/pkg/utils"
+	"os"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -12,5 +14,47 @@ var _ = Describe("Common tests", func() {
 	It("should get correct datamodel name from crd", func() {
 		datamodelName := utils.GetDatamodelName("datamodels.nexus.org")
 		Expect(datamodelName).To(Equal("nexus.org"))
+	})
+
+	It("should check if file exist", func() {
+		file, err := os.Create("test-file.txt")
+		Expect(err).ToNot(HaveOccurred())
+
+		check := utils.IsFileExists(file.Name())
+		Expect(check).To(BeTrue())
+
+		err = os.Remove("test-file.txt")
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("should check if file not exist", func() {
+		check := utils.IsFileExists("non-existent-file")
+		Expect(check).To(BeFalse())
+	})
+
+	It("should check if server config is valid", func() {
+		isValid := utils.IsServerConfigValid(&config.Config{
+			Server: config.ServerConfig{
+				Address:  "address",
+				CertPath: "cert_path",
+				KeyPath:  "key_path",
+			},
+		})
+		Expect(isValid).To(BeTrue())
+	})
+
+	It("should check if server config is not valid", func() {
+		isValid := utils.IsServerConfigValid(&config.Config{})
+		Expect(isValid).To(BeFalse())
+	})
+
+	It("should get crd type", func() {
+		crdType := utils.GetCrdType("Test", "vmware.org")
+		Expect(crdType).To(Equal("tests.vmware.org"))
+	})
+
+	It("should get resource name", func() {
+		resource := utils.GetGroupResourceName("Test")
+		Expect(resource).To(Equal("tests"))
 	})
 })
