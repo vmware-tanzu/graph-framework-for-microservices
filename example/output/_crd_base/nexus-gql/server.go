f04225509f24ce9a165f1c89142ee6319e3f3a92
Pavithra S <pavithras@pavithras-a01.vmware.com>
2022-09-26 21:20:21 +0530

    Fix graphql server

--- example/output/_crd_base/nexus-gql/server.go
+++ example/output/_crd_base/nexus-gql/server.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"nexustempmodule/nexus-gql/graph"
-	"nexustempmodule/nexus-gql/graph/generated"
-	"gitlab.eng.vmware.com/nsx-allspark_users/nexus-sdk/gqlgen.git/graphql"
 	"gitlab.eng.vmware.com/nsx-allspark_users/nexus-sdk/gqlgen.git/graphql/handler"
 	"gitlab.eng.vmware.com/nsx-allspark_users/nexus-sdk/gqlgen.git/graphql/playground"
 	"net/http"
+	"nexustempmodule/nexus-gql/graph"
+	"nexustempmodule/nexus-gql/graph/generated"
 )
-
 
 func StartHttpServer() {
 	ES := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
 	Hander_server := handler.NewDefaultServer(ES)
-	HttpHandlerFunc = playground.Handler("GraphQL playground", "/query")
+	HttpHandlerFunc := playground.Handler("GraphQL playground", "/query")
 	http.Handle("/", HttpHandlerFunc)
 	http.Handle("/query", Hander_server)
 }
-
-
