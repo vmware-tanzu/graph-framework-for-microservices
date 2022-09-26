ebf2e1e2dbe5188f2746da1302dd810458c16a10
Pavithra S <pavithras@pavithras-a01.vmware.com>
2022-09-26 22:01:10 +0530

    Fix imports

--- example/output/_crd_base/nexus-gql/server.go
+++ example/output/_crd_base/nexus-gql/server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"nexustempmodule/nexus-gql/graph"
+	"nexustempmodule/nexus-gql/graph/generated"
+	"gitlab.eng.vmware.com/nsx-allspark_users/nexus-sdk/gqlgen.git/graphql"
 	"gitlab.eng.vmware.com/nsx-allspark_users/nexus-sdk/gqlgen.git/graphql/handler"
 	"gitlab.eng.vmware.com/nsx-allspark_users/nexus-sdk/gqlgen.git/graphql/playground"
 	"net/http"
-	"nexustempmodule/nexus-gql/graph"
-	"nexustempmodule/nexus-gql/graph/generated"
 )
+
 
 func StartHttpServer() {
 	ES := generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}})
