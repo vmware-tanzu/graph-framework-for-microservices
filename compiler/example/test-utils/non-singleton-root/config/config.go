fd00dc288ef19abf3c959adf03444e8e7cca8bc4
Marcin Kulawczuk <mkulawczuk@vmware.com>
2022-10-25 11:01:59 +0200

    fix wrong updates

--- compiler/example/test-utils/non-singleton-root/config/config.go
+++ compiler/example/test-utils/non-singleton-root/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"github.com/vmware-tanzu/graph-framework-for-microservices/compiler/example/test-utils/non-singleton-root/nexus"
+	"github.com/vmware-tanzu/graph-framework-for-microservices/common-library/pkg/nexus"
 )
 
 type Config struct {
