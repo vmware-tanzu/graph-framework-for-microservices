9cd18536f77cb8a8a1e61b8572a9997c2ca8f73c
klokeshvar <klokeshvar@vmware.com>
2022-10-26 11:20:05 +0530

    Addressed review comments

--- compiler/pkg/parser/reserved_names.go
+++ compiler/pkg/parser/reserved_names.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"go/ast"
 	"strings"
 
@@ -10,29 +11,33 @@ import (
 var ReservedTypeNames = []string{"ResourceVersion", "SchemeGroupVersion", "Kind", "Resource", "SchemeBuilder",
 	"AddToScheme", "Child", "Link", "NexusStatus", "Id"}
 
-func CheckIfReserved(name string) {
+func CheckIfReserved(name string) error {
 	for _, reservedName := range ReservedTypeNames {
 		if strings.EqualFold(name, reservedName) {
-			log.Fatalf("Name %s is reserved. Please change type name from %s to something else", reservedName, name)
+			return fmt.Errorf("name %s is reserved", reservedName)
 		}
 	}
+	return nil
 }
 
 func CheckIfFieldsAreReserved(s *ast.StructType) {
 	for _, f := range s.Fields.List {
-		if val, ok := f.Type.(*ast.StructType); ok {
-			CheckIfFieldsAreReserved(val)
-		}
 		name, err := GetFieldName(f)
 		if err != nil {
 			log.Fatalf("failed to determine field name: %v", err)
 		}
-		CheckIfReserved(name)
+		err = CheckIfReserved(name)
+		if err != nil {
+			log.Fatalf("Please change the field name %s as %s ", name, err.Error())
+		}
 	}
 }
 
 func CheckIfNameReserved(n *ast.TypeSpec) {
-	CheckIfReserved(n.Name.Name)
+	err := CheckIfReserved(n.Name.Name)
+	if err != nil {
+		log.Fatalf("Please change the type name %s as %s ", n.Name.Name, err.Error())
+	}
 	if val, ok := n.Type.(*ast.StructType); ok {
 		CheckIfFieldsAreReserved(val)
 	}
