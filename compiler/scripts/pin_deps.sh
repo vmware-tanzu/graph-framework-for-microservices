1d667d559ac7a7fae42a79e573035eb7bb93c5b9
Janakiraman <ramramu.jr@gmail.com>
2022-10-27 21:17:36 +0530

    NPT-566 Go mod replace with COMPILER_DIRECTORY/.. (#18)
    go mod edit in pin deps is pointing to relative path which fails in build folder when calling go build for graphql due to the relative path
    due to build being run from _generated folder we ran into runtime panic
    
    replace with absolute path to fix issues
    by running graphql build from nexustempmodule folder to remove this errors

--- compiler/scripts/pin_deps.sh
+++ compiler/scripts/pin_deps.sh
@@ -16,6 +16,8 @@ if [[ -z $DEFAULT_CLIENT_VERSION_TAG ]]; then
   exit 1
 fi
 
+COMPILER_SRC_DIRECTORY=$1
+
 go mod edit -require github.com/elliotchance/orderedmap@v1.4.0
 go mod edit -require k8s.io/apimachinery@$DEFAULT_CLIENT_VERSION_TAG
 go mod edit -require k8s.io/client-go@$DEFAULT_CLIENT_VERSION_TAG
@@ -25,6 +27,6 @@ go mod edit -replace k8s.io/apiextensions-apiserver=k8s.io/apiextensions-apiserv
 go mod edit -replace k8s.io/apimachinery=k8s.io/apimachinery@$DEFAULT_CLIENT_VERSION_TAG
 go mod edit -replace k8s.io/client-go=k8s.io/client-go@$DEFAULT_CLIENT_VERSION_TAG
 
-go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/gqlgen=../../gqlgen
-go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/kube-openapi=../../kube-openapi
-go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/nexus=../../nexus
+go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/gqlgen=${COMPILER_SRC_DIRECTORY}/../gqlgen
+go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/kube-openapi=${COMPILER_SRC_DIRECTORY}/../kube-openapi
+go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/nexus=${COMPILER_SRC_DIRECTORY}/../nexus
