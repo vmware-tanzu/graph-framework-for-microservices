a8e1f9fac4689e5d90ae72257652ed3aa6e414d8
rjanakiraman <rjanakiraman@vmware.com>
2022-11-03 15:09:20 +0530

    debug

--- compiler/scripts/pin_graphql_deps.sh
+++ compiler/scripts/pin_graphql_deps.sh
@@ -8,3 +8,4 @@ go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/g
 go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/kube-openapi=${COMPILER_SRC_DIRECTORY}/../kube-openapi
 go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/nexus=${COMPILER_SRC_DIRECTORY}/../nexus
 go mod edit -require github.com/cespare/xxhash/v2@v2.1.2
+go mod edit -require github.com/imdario/mergo@v0.3.12
