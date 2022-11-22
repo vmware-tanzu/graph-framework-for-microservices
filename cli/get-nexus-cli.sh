02980fdf55aa05f7226c8f507f74c7b0c12d48d6
sankarm <sankarm@vmware.com>
2022-11-22 10:21:24 +0530

    default bin path changed to PWD and added log message

--- cli/get-nexus-cli.sh
+++ cli/get-nexus-cli.sh
@@ -23,11 +23,13 @@ while [[ $# -gt 0 ]]; do
        exit 0
        ;;
     *)
-       echo "Input not understood. See '--help' for information on using this command."
+       echo "Input is invalid. See '--help' for information on using this command."
        exit 1
        ;;
   esac
 done
+
+default_path=""
 
 if [ -z "$REPOSITORY" ]
 then
@@ -36,13 +38,13 @@ fi
 
 if [ -z "$VERSION" ]
 then
-   #VERSION="v0.0.134"
    VERSION="latest"
 fi
 
 if [ -z "$DST_DIR" ]
 then
-   DST_DIR="/usr/local/bin"
+   default_path="$PWD"
+   DST_DIR="$default_path"
 fi
 
 OS=`uname -s`
@@ -51,15 +53,23 @@ docker_name="nexus-cli"
 darwin_src_path="/nexus/darwin/nexus"
 linux_src_path="/nexus/linux/nexus"
 
-docker create --name "$docker_name" ${REPOSITORY}:${VERSION}
+docker create --name "$docker_name" ${REPOSITORY}:${VERSION} 1> /dev/null
 
 if [ "$OS" == "Darwin" ]; then
    docker cp "${docker_name}:${darwin_src_path}" "$DST_DIR/nexus" 
 elif [ "$OS" == "Linux" ]; then
-   docker cp "${docker_name}:${linux_src_path}" "$DST_DIR/nexus" 
+   docker cp "${docker_name}:${linux_src_path}" "$DST_DIR/nexus"
 else
    echo "Unsupported OS type: ${OS}, nexus cli supported OS types are : 1) Linux and 2) Darwin"
 fi
 
-docker rm -f ${docker_name}
+docker rm -f ${docker_name} 1> /dev/null
+
+if [ -n "$default_path" ]
+then
+    echo -e "The nexus cli binary downloaded here: $default_path/nexus\nPlease place the nexus cli binary in the User PATH, hence it's accessible from anywhere in the shell"
+else
+    echo "The nexus cli binary downloaded here: $DST_DIR/nexus"
+fi
+
 
