5428ff436cbe7ca047cfcb906c701f273ca4fd9c
vishnuk <kvishnu@vmware.com>
2022-12-01 12:44:32 +0530

    TSM-714: Removed unused files and fixed vulnerability (#89)
    Fixed Gqlgen library dependencies vulnerability

--- gqlgen/_examples/tools.go
+++
@@ -1,9 +0,0 @@
-//go:build tools
-// +build tools
-
-package main
-
-import (
-	_ "github.com/vektah/dataloaden"
-	_ "golang.org/x/text"
-)
--- gqlgen/integration/models-go/viewer.go
+++
@@ -1,7 +0,0 @@
-package models
-
-import "github.com/vmware-tanzu/graph-framework-for-microservices/gqlgen/integration/remote_api"
-
-type Viewer struct {
-	User *remote_api.User
-}
