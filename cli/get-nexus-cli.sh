de7a3269a29f2748be3a21d2ae6c354a94b892b3
sankarm <sankarm@vmware.com>
2022-12-01 09:56:16 +0530

    review comment: modified the interactive messages and inputs

--- cli/get-nexus-cli.sh
+++ cli/get-nexus-cli.sh
@@ -3,23 +3,22 @@ set -e
 
 REPOSITORY="gcr.io/nsx-sm/nexus/nexus-cli"
 VERSION="latest"
-DST_DIR="$PWD"
-CURR_NEXUS_PATH=$(which nexus) || true
+DST_DIR="/usr/local/bin"
 
 usage() { echo "Usage: $0 [-r <repository-name>] [-v <version>] [-d <destination-path>] " 1>&2; exit 1; }
 
 if [[ $# == 0 ]]; then
-    echo "Installing nexus cli"
-    echo -n "Enter the nexus cli image repository(${REPOSITORY}):"
-    read -r repository_name
-    echo -n "Enter the nexus cli version to be installed(${VERSION}):"
-    read -r version
-    if [[ -n ${CURR_NEXUS_PATH} ]]; then
-       echo -n "Do you want to over-write the existing nexus cli(${CURR_NEXUS_PATH} [yes/no]):"
-       read -r overwrite_path
-    else
-       echo -n "Enter the Path to install nexus cli(${DST_DIR}):"
-       read -r dest_path
+    echo -e "Downloading Nexus ...\nVersion: ${VERSION}\nImage repository: ${REPOSITORY}\nDirectory: ${DST_DIR}\n"
+    echo -e "Would you like to customize installation [y/n]:"
+    read -r customize
+
+    if [ "${customize}" == "y" ]; then
+        echo -n "Image repository [${REPOSITORY}]:"
+        read -r repository_name
+        echo -n "Version [${VERSION}]:"
+        read -r version
+        echo -n "Directory [/usr/local/bin]:"
+        read -r dest_path
     fi
 else
     while getopts ":r:v:d:" o; do
@@ -52,18 +51,9 @@ fi
 
 if [[ -n "${dest_path}" ]]; then
     DST_DIR="${dest_path}"
-elif [[ "${overwrite_path}" == "yes" ]]; then
-    
-    DST_DIR=`echo "${CURR_NEXUS_PATH}" | awk -F'/' 'BEGIN{OFS=FS} {$NF=""; print}'`
-elif [[ "${overwrite_path}" == "no" ]]; then
-    DST_DIR="${PWD}"
 fi
 
-echo "The nexus CLI will be downloaded from ${REPOSITORY}:${VERSION} and get installed to ${DST_DIR}"
-
-
 OS=`uname -s`
-
 docker_name="nexus-cli"
 darwin_src_path="/nexus/darwin/nexus"
 linux_src_path="/nexus/linux/nexus"
@@ -78,11 +68,11 @@ if [ "$OS" == "Darwin" ]; then
 elif [ "$OS" == "Linux" ]; then
    docker cp "${docker_name}:${linux_src_path}" "$DST_DIR"
 else
-   echo "Unsupported OS type: ${OS}, nexus cli supported OS types are : 1) Linux and 2) Darwin"
+   echo "Unsupported OS type: ${OS}, nexus supported OS types are : 1) Linux and 2) Darwin"
 fi
 
 docker rm -f ${docker_name} 1> /dev/null
 docker rmi "${REPOSITORY}":"${VERSION}" &> /dev/null 
 
-echo "The nexus cli binary downloaded here: $DST_DIR/nexus"
+echo -e "Nexus (${VERSION}) installed in $DST_DIR/nexus\nRun \"nexus help\" to get started"
 
