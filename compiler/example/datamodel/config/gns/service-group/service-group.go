2c3f8cb3b45d0cc04e147202eec4a17c01d5c13d
klokeshvar <klokeshvar@vmware.com>
2023-01-04 11:10:26 +0530

    Removing one comment from example datamodel that is no longer relevant

--- compiler/example/datamodel/config/gns/service-group/service-group.go
+++ compiler/example/datamodel/config/gns/service-group/service-group.go
@@ -10,8 +10,6 @@ type SvcGroup struct {
 	DisplayName string
 	Description string
 	Color       string
-	// TODO support links which are not nexus nodes https://jira.eng.vmware.com/browse/NPT-112
-	//Services    core_v1.Service `nexus:"links"`
 }
 
 type SvcGroupLinkInfo struct {
