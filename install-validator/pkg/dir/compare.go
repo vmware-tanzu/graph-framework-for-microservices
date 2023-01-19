298512f71009280b6064df005f57243d4528398f
rafal-slominskii <rafals@vmware.com>
2023-01-19 13:50:11 +0100

    add debug logs

--- install-validator/pkg/dir/compare.go
+++ install-validator/pkg/dir/compare.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/sirupsen/logrus"
+
 	nexuscompare "github.com/vmware-tanzu/graph-framework-for-microservices/common-library/pkg/nexus-compare"
 	kubewrapper "github.com/vmware-tanzu/graph-framework-for-microservices/install-validator/pkg/k8s-utils"
 	"sigs.k8s.io/yaml"
@@ -25,9 +27,11 @@ func CheckDir(dir string, c kubewrapper.ClientInt, cFunc compareFunc) (map[strin
 			return err
 		}
 		if info.IsDir() {
+			logrus.Debugf("%s is a directory, skipping", info.Name())
 			return nil
 		}
 		if !strings.HasSuffix(info.Name(), ".yaml") {
+			logrus.Debugf("%s is not a yaml file, skipping", info.Name())
 			return nil
 		}
 		newData, err := os.ReadFile(path)
@@ -42,6 +46,7 @@ func CheckDir(dir string, c kubewrapper.ClientInt, cFunc compareFunc) (map[strin
 		crd := c.GetCrd(name)
 		toInstall = append(toInstall, name)
 		if crd == nil { //the crd is New, so no incompatible changes
+			logrus.Debugf("%s not found in cluster, skipping comparison", name)
 			return nil
 		}
 
--- install-validator/pkg/dir/install.go
+++ install-validator/pkg/dir/install.go
@@ -5,6 +5,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/sirupsen/logrus"
+
 	kubewrapper "github.com/vmware-tanzu/graph-framework-for-microservices/install-validator/pkg/k8s-utils"
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"sigs.k8s.io/yaml"
@@ -13,16 +15,20 @@ import (
 func InstallDir(dir string, c kubewrapper.ClientInt) error {
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
+			logrus.Debugf("unexpected error while walking through dir: %s ", dir)
 			return err
 		}
 		if info.IsDir() {
+			logrus.Debugf("%s is a directory, skipping", info.Name())
 			return nil
 		}
 		if !strings.HasSuffix(info.Name(), ".yaml") {
+			logrus.Debugf("%s is not a yaml file, skipping", info.Name())
 			return nil
 		}
 		data, err := os.ReadFile(path)
 		if err != nil {
+			logrus.Debugf("error while reading file %s", info.Name())
 			return err
 		}
 
