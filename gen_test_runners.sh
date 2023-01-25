21277c692ede645255589fefb473c17953b85980
Ajay Y Bharadwaj <bajayyadhave@vmware.com>
2023-01-25 11:30:52 +0530

    Minor code cleanup

--- gen_test_runners.sh
+++ gen_test_runners.sh
@@ -1,19 +1,19 @@
-for i in {91..100} 
+for i in {1..100} 
 do
 var1='{
     "server": {
-        "url": "http://nexus-api-gw.nexus5",
+        "url": "http://nexus-api-gw.scaletest9",
         "zipkin": "http://zipkin:9411",
-        "tsdb": "postgres://postgres:dXmrYXVfwgD2JZvl@tsdb.timescale:5432/testdb"
+        "tsdb": "postgres://postgres:Ordjdgz9zd9Hii07@tsdb.timescale:5432/testdb"
     },
     "tests": [
         {
             "name": "write_n_objects",
             "concurrency": 1,
-            "ops_count": 1000,
-            "sample_rate": 0.1,
+            "ops_count": 1,
+            "sample_rate": 1.0,
             "rest": [
-                "put_operations'$i'"
+                "put_operations10"
             ]
         }
     ]
@@ -22,5 +22,5 @@ var1='{
 echo curl --location --request POST 'localhost:8000/tests/t'$i \
 --header \'Content-Type: application/json\' \
 --data-raw \'$var1\'
-echo sleep 1
+echo sleep 10
 done
