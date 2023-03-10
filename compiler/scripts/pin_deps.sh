f8168d8cdba62d9096d7d2672324713f55f203c9
Janakiraman <ramramu.jr@gmail.com>
2023-03-10 15:27:52 +0530

    NPT-632 Pinning deps for graphql plugin (#134)
    * debug logs
    
    * pin utils
    
    * pin client-go and x/time
    
    ---------
    
    Co-authored-by: rjanakiraman <rjanakiraman@vmware.com>

--- compiler/scripts/pin_deps.sh
+++ compiler/scripts/pin_deps.sh
@@ -44,4 +44,4 @@ go mod edit -require golang.org/x/oauth2@v0.0.0-20220411215720-9780585627b5
 go mod edit -require k8s.io/klog/v2@v2.70.1
 go mod edit -require golang.org/x/net@v0.2.0
 go mod edit -require google.golang.org/grpc@v1.51.0
-
+go mod edit -require k8s.io/utils@v0.0.0-20221128185143-99ec85e7a448
--- compiler/scripts/pin_graphql_build_version.sh
+++ compiler/scripts/pin_graphql_build_version.sh
@@ -9,13 +9,14 @@ go mod edit -replace github.com/vmware-tanzu/graph-framework-for-microservices/n
 go mod edit -require github.com/cespare/xxhash/v2@v2.1.2
 go mod edit -require github.com/google/gofuzz@v1.1.0
 go mod edit -require github.com/imdario/mergo@v0.3.12
-go mod edit -require k8s.io/apimachinery@v0.25.3
-go mod edit -require k8s.io/client-go@v0.25.3
+go mod edit -require k8s.io/apimachinery@v0.26.0
+go mod edit -require k8s.io/client-go@v0.26.0
 go mod edit -require golang.org/x/sys@v0.2.0
-go mod edit -require golang.org/x/time@v0.0.0-20220210224613-90d013bbcef8
+go mod edit -require golang.org/x/time@v0.3.0
 go mod edit -require golang.org/x/term@v0.2.0
 go mod edit -require golang.org/x/text@v0.4.0
 go mod edit -require golang.org/x/oauth2@v0.0.0-20220411215720-9780585627b5
 go mod edit -require k8s.io/klog/v2@v2.70.1
 go mod edit -require golang.org/x/net@v0.2.0
 go mod edit -require google.golang.org/grpc@v1.51.0
+go mod edit -require k8s.io/utils@v0.0.0-20221128185143-99ec85e7a448
