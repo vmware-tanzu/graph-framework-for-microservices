7af724ffff8fabb97e0b44f49aa946d8bfb51278
vipinakavarpu <vakavarpu@vmware.com>
2023-03-29 15:09:10 -0400

    skipping directories logic

--- compiler/pkg/preparser/preparser.go
+++ compiler/pkg/preparser/preparser.go
@@ -3,6 +3,7 @@ package preparser
 import (
 	"bytes"
 	"fmt"
+	"github.com/vmware-tanzu/graph-framework-for-microservices/compiler/pkg/config"
 	"go/ast"
 	goParser "go/parser"
 	"go/printer"
@@ -34,6 +35,12 @@ func Parse(startPath string) map[string][]*parser.Package {
 			if info.Name() == "vendor" {
 				log.Infof("Ignoring vendor directory...")
 				return filepath.SkipDir
+			}
+			for _, f := range config.ConfigInstance.IgnoredDirs {
+				if info.Name() == f {
+					log.Infof(fmt.Sprintf("Ignoring %v directory from config", f))
+					return filepath.SkipDir
+				}
 			}
 
 			fileset := token.NewFileSet()
